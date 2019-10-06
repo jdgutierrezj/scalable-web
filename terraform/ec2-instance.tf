16c5098218f60a4ed6bc1de003ba2500c2a5fa12
Jeison Gutierrez <jgutierrez@cobiscorp.com>
2019-10-06 12:52:00 -0500

    Initial Commit

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -46,6 +46,13 @@ resource "aws_security_group" "allow_8080_22" {
 	  cidr_blocks = ["0.0.0.0/0"]
 	}
 	
+	egress {
+	  from_port       = 0
+	  to_port         = 0
+	  protocol        = "-1"
+	  cidr_blocks     = ["0.0.0.0/0"]
+	}
+	
 	tags = {
 	  Name = "allow_8080_22"
 	}
@@ -57,7 +64,7 @@ resource "aws_instance" "waes_jgu" {
 	instance_type               = "t2.micro"
 	vpc_security_group_ids      = ["${aws_security_group.allow_8080_22.id}"]
 	key_name					= "jgu-keypair"
-	user_data   		    = "${file("install_scalable_web.sh")}"
+	user_data   		    	= "${file("install_scalable_web.sh")}"
     
     tags = {
     	Name = "WaesServer"
