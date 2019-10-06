9fedcac0c1e2d998c67c10b8693d91e0afc5d726
Jeison Gutierrez <jgutierrez@cobiscorp.com>
2019-10-06 13:04:36 -0500

    Initial Commit

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -33,20 +33,6 @@ resource "aws_security_group" "allow_8080_22" {
 	}
 	
 	egress {
-	  from_port   = 8080
-	  to_port     = 8080
-	  protocol    = "tcp"
-	  cidr_blocks = ["0.0.0.0/0"]
-	}
-	
-	egress {
-	  from_port   = 22
-	  to_port     = 22
-	  protocol    = "tcp"
-	  cidr_blocks = ["0.0.0.0/0"]
-	}
-	
-	egress {
 	  from_port       = 0
 	  to_port         = 0
 	  protocol        = "-1"
--- terraform/install_scalable_web.sh
+++ terraform/install_scalable_web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum update -y
-yum install java-1.8.0-openjdk.x86_64 apache-maven git -y
-git clone https://github.com/jdgutierrezj/scalable-web.git
+yum install java-1.8.0-openjdk.x86_64 maven git -y
+git clone https://github.com/jdgutierrezj/scalable-web
 cd scalable-web
 mvn clean install
 java -jar target/scalable-web-1.0.0.jar -Dspring.profiles.active=prod
