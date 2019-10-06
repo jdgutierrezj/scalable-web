eded30f63ffeb42e413166a6b6395566ced34e91
Jeison Gutierrez <jgutierrez@cobiscorp.com>
2019-10-06 11:25:58 -0500

    Initial Commit

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -1,14 +1,45 @@
 data "aws_ami" "amazon-linux-2" {
- most_recent = true
+	most_recent = true
+	
+	filter {
+	   name   = "owner-alias"
+	   values = ["amazon"]
+	}
+	
+	
+	filter {
+	 name   = "name"
+	 values = ["amzn2-ami-hvm*"]
+	}
+}
 
- filter {
-   name   = "owner-alias"
-   values = ["amazon"]
- }
+resource "aws_security_group" "allow_8080" {
+	name        = "allow_8080"
+	description = "Allow HTTP inbound traffic"
 
+	ingress {
+	  from_port   = 8080
+	  to_port     = 8080
+	  protocol    = "tcp"
+	  cidr_blocks = 0.0.0.0/0
+	}
+	
+	tags = {
+	  Name = "allow_8080"
+	}
+}
 
- filter {
-   name   = "name"
-   values = ["amzn2-ami-hvm*"]
- }
+resource "aws_instance" "waes_jgu" {
+	ami                         = "${data.aws_ami.amazon-linux-2.id}"
+	associate_public_ip_address = true
+	instance_type               = "t2.micro"
+	vpc_security_group_ids      = ["${aws_security_group.allow_8080.id}"]
+	subnet_id                   = "${aws_subnet.test.id}"
+	user_data					= "#!/bin/bash -x"
+								  "yum update -y
+       							   yum install -y java-1.8.0-openjdk.x86_64 docker git
+       							   git clone https://github.com/jdgutierrezj/scalable-web.git
+       							   cd scalable-web
+       							   mvn clean install
+       							   java -jar target/scalable-web-1.0.0.jar -Dspring.profiles.active=prod"
 }
---
+++ terraform/outputs.tf
@@ -0,0 +1,3 @@
+output "ip" {
+  value = aws_instance.waes_jgu.public_ip
+}
